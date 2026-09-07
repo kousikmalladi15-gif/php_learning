6611739003aec21a73873e2ee65038004cc992e5
Krishna Kousik <krishnakousik.innovateiq@gmail.com>
2026-09-07 15:47:24 +0530

    basic array methods have been learnt and exists in the .php file, will learn more soon and update

--- array methods.php
+++ array methods.php
@@ -1,12 +1,40 @@
 <?php
 
-$arrayh=[1,5,2];
-echo(count($arrayh).PHP_EOL);
-array_shift($arrayh);
-//array_shift(array)
-echo(implode(', ',$arrayh).PHP_EOL);
-//implode(str_in_between, array)
-//array_unshift(array, values)
-array_unshift($arrayh, 1,5);
-echo(implode(", ",$arrayh).PHP_EOL);
+$arr=['Kousik', 'Kavya'];
+
+$names=implode(' ', $arr);
+// converts the array into string
+
+// $val=array_find($arr, fn($str)=>$str==='Kavya');
+// echo $val;
+// introduced in php 8.4, second arg is callable i.e. function;
+
+$search="KaVya";
+$found = in_array(strtolower($search), array_map('strtolower', $arr));
+//in_array checks whether a value exists in the array or not.
+if($found){
+    echo "Found ". strtolower($search).PHP_EOL;
+} 
+
+$lowerarr=implode(', ', array_map('strtolower', $arr));
+
+echo $lowerarr .PHP_EOL;
+
+//array_map maps a method to all the values of an array
+
+$arr[]='Amma';
+echo implode(' ', $arr).PHP_EOL;
+// adding new elements with array[]=value;
+
+array_splice($arr, 3, 0, "Nanna");
+echo implode(' ', $arr).PHP_EOL;
+echo str_repeat('-',20).PHP_EOL;
+
+
+$ordered_family=array_reverse($arr);
+echo implode(', ', $ordered_family).PHP_EOL;
+
+array_shift($ordered_family);
+
+echo implode(' ',$ordered_family);
 ?>
